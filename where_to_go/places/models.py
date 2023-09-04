a7a58f2c6c2e4b5044cddcccc3c4328eb29d7407
Vladimir Papin <vladimir.papin@gmail.com>
2023-09-04 18:34:55 +0300

    Add Point model

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Point(models.Model):
+    title = models.CharField(
+        max_length=150,
+        verbose_name='Название')
+
+    description_short = models.CharField(
+        verbose_name='Короткое описание',
+        max_length=500,
+        db_index=True)
+
+    description_long = models.TextField(
+        verbose_name='Описание')
+
+    longitude = models.DecimalField(
+        verbose_name='Долгота',
+        max_digits=16,
+        decimal_places=14)
+
+    latitude = models.DecimalField(
+        verbose_name='Широта',
+        max_digits=16,
+        decimal_places=14)
+
+    class Meta:
+        verbose_name = 'Места'
+        verbose_name_plural = 'Места'
+
+    def __str__(self):
+        return self.title
