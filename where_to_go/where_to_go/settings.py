f24489699ee801661a06122c0f2001074472f1e5
Vladimir Papin <vladimir.papin@gmail.com>
2023-09-06 18:25:36 +0300

    Setup media files serving

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -122,11 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+MEDIA_ROOT = 'media/'
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from where_to_go import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.map_poster),
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
