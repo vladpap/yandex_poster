0b5f82154198ab4243f6da26b8a9a431162660d4
Vladimir Papin <vladimir.papin@gmail.com>
2023-09-07 16:22:43 +0300

    Move data from template to view

--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -2,4 +2,37 @@ from django.shortcuts import render
 
 
 def map_poster(request):
-    return render(request, 'index.html')
+
+    places_geojson = {
+    "type": "FeatureCollection",
+        "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [37.62, 55.793676]
+            },
+            "properties": {
+                "title": "«Легенды Москвы",
+                "placeId": "moscow_legends",
+                "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [37.64, 55.753676]
+            },
+            "properties": {
+                "title": "Крыши24.рф",
+                "placeId": "roofs24",
+                "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
+            }
+        }
+        ]
+        }
+    return render(
+        request,
+        'index.html',
+        context={'places_geojson': places_geojson})
