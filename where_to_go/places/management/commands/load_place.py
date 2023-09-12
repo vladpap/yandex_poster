142996f4fcf216f2171728079fedc25328b7790d
Vladimir Papin <vladimir.papin@gmail.com>
2023-09-12 09:14:10 +0300

    added command load_place

---
+++ where_to_go/places/management/commands/load_place.py
@@ -0,0 +1,56 @@
+from django.core.management.base import BaseCommand
+
+import requests
+from tqdm import tqdm
+
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
+
+from ...models import Point, Image
+
+import os
+from urllib.parse import urlparse
+
+
+def tqdm_enumerate(iter):
+    i = 0
+    for y in tqdm(iter):
+        yield i, y
+        i += 1
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        response = requests.get(options['url_json_position'])
+        response.raise_for_status()
+        point_from_url = response.json()
+        point, created = Point.objects.get_or_create(
+            title=point_from_url['title'],
+            description_short=point_from_url['description_short'],
+            description_long=point_from_url['description_long'],
+            longitude=point_from_url['coordinates']['lng'],
+            latitude=point_from_url['coordinates']['lat']
+            )
+
+        for index, image_url in tqdm_enumerate(point_from_url['imgs']):
+            response = requests.get(image_url)
+            response.raise_for_status()
+
+            img_temp = NamedTemporaryFile(delete=True)
+            img_temp.write(response.content)
+            img_temp.flush()
+
+            file_name = os.path.basename(urlparse(image_url).path)
+            image = Image()
+            image.image.save(file_name,
+                             File(img_temp),
+                             save=False)
+            image.to_point = point
+            image.position = index
+            image.save()
+
+    def add_arguments(self, parser):
+        parser.add_argument('url_json_position',
+                            type=str,
+                            help='URL данных в json, информации о локации'
+                            )
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path('places/<int:post_id>', views.poster_detail_view),
     path('tinymce/', include('tinymce.urls')),
     ]
-    
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
+urlpatterns += [re_path(r'^media/(?P<path>.*)$',
+                serve,
+                {'document_root': settings.MEDIA_ROOT, }), ]
