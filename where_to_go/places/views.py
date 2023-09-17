b489cb5bb34e0d68dc89ab38f3414df53f05a5b6
Vladimir Papin <vladimir.papin@gmail.com>
2023-09-17 18:29:17 +0300

    moved views in place app

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,2 +1,55 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from places.models import Point
 
-# Create your views here.
+
+def get_point_from_id(id):
+
+    point = Point.objects.filter(id=id).first()
+    point_images = []
+    for image in point.images.all():
+        point_images.append(f'.{image.image.url}')
+
+    return {
+        "title": point.title,
+        "imgs": point_images,
+        "description_short": point.short_description,
+        "description_long": point.long_description,
+        "coordinates": {
+            "lng": point.longitude,
+            "lat": point.latitude
+            }
+    }
+
+
+def map_poster(request):
+
+    points = []
+    points_set = Point.objects.all()
+    for point_set in points_set:
+        point = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [point_set.longitude, point_set.latitude]
+            },
+            'properties': {
+                'title': point_set.title,
+                'placeId': point_set.id,
+                'detailsUrl': f'/places/{point_set.id}'
+            }
+        }
+        points.append(point)
+
+    places_geojson = {
+        'type': 'FeatureCollection',
+        'features': points
+        }
+    return render(
+        request,
+        'index.html',
+        context={'places_geojson': places_geojson})
+
+
+def poster_detail_view(request, post_id):
+    return JsonResponse(get_point_from_id(post_id))
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
 
-from where_to_go import views
+from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
