ff540b65aaa9ac9ac180210e26ad6cef9b5f6bc6
Vladimir Papin <vladimir.papin@gmail.com>
2023-09-17 22:49:32 +0300

    shortened the code

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -1,9 +1,8 @@
 import os
-from urllib.parse import urlparse
+import urllib
 
 import requests
 from django.core.files import File
-from django.core.files.temp import NamedTemporaryFile
 from django.core.management.base import BaseCommand
 from tqdm import tqdm
 
@@ -24,28 +23,22 @@ class Command(BaseCommand):
         point_from_url = response.json()
         point, created = Point.objects.get_or_create(
             title=point_from_url['title'],
-            description_short=point_from_url['description_short'],
-            description_long=point_from_url['description_long'],
+            short_description=point_from_url['description_short'],
+            long_description=point_from_url['description_long'],
             longitude=point_from_url['coordinates']['lng'],
             latitude=point_from_url['coordinates']['lat']
             )
 
         for index, image_url in tqdm_enumerate(point_from_url['imgs']):
-            response = requests.get(image_url)
-            response.raise_for_status()
-
-            img_temp = NamedTemporaryFile(delete=True)
-            img_temp.write(response.content)
-            img_temp.flush()
-
-            file_name = os.path.basename(urlparse(image_url).path)
             image = Image()
-            image.image.save(file_name,
-                             File(img_temp),
-                             save=False)
             image.to_point = point
             image.position = index
-            image.save()
+            image.image.save(
+                os.path.basename(image_url),
+                File(open(urllib.request.urlretrieve(image_url)[0],
+                          mode='rb')
+                     )
+                )
 
     def add_arguments(self, parser):
         parser.add_argument('url_json_position',
